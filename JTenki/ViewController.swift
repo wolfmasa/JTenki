02340f97ec9c002cfdbe1ecaec298d988e958204
Masa Jobara <wolf.masa@gmail.com>
2015-03-22 10:55:20 +0900

    Initial Commit

---
+++ JTenki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JTenki
+//
+//  Created by JobaraMasashi on 2015/03/22.
+//  Copyright (c) 2015年 ProjectJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
