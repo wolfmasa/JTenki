78652ade0173253b9d9b4a3a003c5b0199e8f201
wolfmasa <wolf.masa@gmail.com>
2015-03-23 18:50:01 +0900

    add J league Team

---
+++ JTenki/JTableView.swift
@@ -0,0 +1,106 @@
+//
+//  JTableView.swift
+//  JTenki
+//
+//  Created by ICP223G on 2015/03/23.
+//  Copyright (c) 2015年 ProjectJ. All rights reserved.
+//
+
+import UIKit
+
+struct Teams {
+    var label: String
+    var imageName: String?
+    var location: String
+}
+
+
+class JTableView: UITableViewController {
+
+    var teamArray: Array<Teams> = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        teamArray.append(Teams(label: "鹿島アントラーズ", imageName: nil, location: "Kashima"))
+        teamArray.append(Teams(label: "ガンバ大阪", imageName: nil, location: "Osaka"))
+        teamArray.append(Teams(label: "横浜・F・マリノス", imageName: nil, location: "Yokohama"))
+        teamArray.append(Teams(label: "サンフレッチェ広島", imageName: nil, location: "Hiroshima"))
+        teamArray.append(Teams(label: "川崎フロンターレ", imageName: nil, location: "Kawasaki"))
+        teamArray.append(Teams(label: "ベガルタ仙台", imageName: nil, location: "Sendai"))
+        teamArray.append(Teams(label: "名古屋グランパス", imageName: nil, location: "Nagoya"))
+        teamArray.append(Teams(label: "FC東京", imageName: nil, location: "Tokyo"))
+        teamArray.append(Teams(label: "柏レイソル", imageName: nil, location: "Kashiwa"))
+        teamArray.append(Teams(label: "湘南ベルマーレ", imageName: nil, location: "Hiratsuka"))
+        teamArray.append(Teams(label: "モンテディオ山形", imageName: nil, location: "Yamagata"))
+        teamArray.append(Teams(label: "ヴィッセル神戸", imageName: nil, location: "Kobe"))
+        teamArray.append(Teams(label: "アルビレックス新潟", imageName: nil, location: "Nigata"))
+
+    }
+
+    func requestToUrl(location :String){
+
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=\(location)"
+
+        // 通信先のURLを生成.
+        var myUrl:NSURL = NSURL(string:url)!
+        // リクエストを生成.
+        var myRequest:NSURLRequest  = NSURLRequest(URL: myUrl)
+        // 送信処理を始める.
+        NSURLConnection.sendAsynchronousRequest(myRequest, queue: NSOperationQueue.mainQueue(), completionHandler: self.getHttp)
+
+    }
+
+    func getHttp(res:NSURLResponse?,data:NSData?,error:NSError?){
+
+        // 帰ってきたデータを文字列に変換.
+        var myData:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+        // TextViewにセット.
+        //textArea.text = myData
+        //println(myData)
+
+        var json:NSDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+
+        let array = json["weather"]! as NSArray
+        let weatherIcon = (array[0] as NSDictionary)["icon"] as NSString
+
+        for( var i=0; i<teamArray.count; i++ ) {
+            var t = teamArray[i]
+            let url = "http://api.openweathermap.org/data/2.5/weather?q=\(t.location)"
+            if url == res?.URL?.absoluteString && teamArray[i].imageName == nil{
+                teamArray[i].imageName = "\(weatherIcon).png"
+                self.tableView.reloadData()
+                break;
+            }
+        }
+
+
+    }
+
+
+//delegate
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Cellの.を取得する.
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as UITableViewCell
+
+        // Cellに値を設定する.
+        cell.textLabel!.text = teamArray[indexPath.row].label
+        if(teamArray[indexPath.row].imageName != nil){
+
+            cell.imageView?.image = UIImage(named: teamArray[indexPath.row].imageName!)
+        }
+
+        requestToUrl(teamArray[indexPath.row].location)
+
+        return cell
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teamArray.count
+    }
+
+}
--- JTenki/ViewController.swift
+++ JTenki/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        requestToUrl("http://api.openweathermap.org/data/2.5/weather?q=London,uk")
+        requestToUrl("http://api.openweathermap.org/data/2.5/weather?q=Kawasaki,jp")
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,69 +53,6 @@ class ViewController: UIViewController {
         println(weather)
     }
 
-/**
-        //NSOperationQueueを使ってマルチスレッドでリクエスト
-        NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-        [queue addOperationWithBlock:^{
-        
-        //////////////////////////////////////
-        //APIからtokenを取得
-        
-        //リクエスト用のパラメータを設定
-        NSString *user = @"user_name";
-        NSString *pass = @"password";
-        NSString *url  = @"request_url";
-        NSString *param = [NSString stringWithFormat:@"url_name=%@&password=%@", user, pass];
-        
-        //リクエストを生成
-        NSMutableURLRequest *request;
-        request = [[NSMutableURLRequest alloc] init];
-        [request setHTTPMethod:@"POST"];
-        [request setURL:[NSURL URLWithString:url]];
-        [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
-        [request setTimeoutInterval:20];
-        [request setHTTPShouldHandleCookies:FALSE];
-        [request setHTTPBody:[param dataUsingEncoding:NSUTF8StringEncoding]];
-        
-        //同期通信で送信
-        NSURLResponse *response = nil;
-        NSError *error = nil;
-        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-        
-        if (error != nil) {
-        NSLog(@"Error!");
-        return;
-        }
-        
-        NSError *e = nil;
-        
-        //取得したレスポンスをJSONパース
-        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:nil error:&e];
-        
-        NSString *token = [dict objectForKey:@"token"];
-        NSLog(@"Token is %@", token);
-        
-        //受け取ったtokenを付けて再度リクエスト
-        NSURL *url2 = [NSURL URLWithString:[NSString stringWithFormat:@"https://requst_url?token=%@", token]];
-        NSURLRequest *request2 = [NSURLRequest requestWithURL:url2];
-        NSURLResponse *response2 = nil;
-        NSError *error2 = nil;
-        NSData *data2 = [NSURLConnection sendSynchronousRequest:request2 returningResponse:&response2 error:&error2];
-        
-        if (error2 != nil) {
-        NSLog(@"Error2!");
-        return;
-        }
-        
-        NSError *e2 = nil;
-        NSDictionary *dict2 = [NSJSONSerialization JSONObjectWithData:data2 options:nil error:&e2];
-        
-        NSLog(@"Response2: %@", response2);
-        NSLog(@"%@", dict2);
-        NSLog(@"%d", [dict2 count]);
-        }];
-    }
-**/
 
 }
 
